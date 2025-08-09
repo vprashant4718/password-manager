fc45730351ed820a3b797a736a782e7a73b61524
Prashant Verma <vprashant4718@gmail.com>
2025-08-09 21:54:36 +0530

    vite config proxy updated

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',  // Your backend
+        target: 'https://password-manager-seven-sigma.vercel.app/',  // Your backend
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''), // remove "/api" prefix
       },
